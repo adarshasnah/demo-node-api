8010607bbc54d2add4c20bd615405087f1bf795f
Adarsh Hasnah <adarshasnah@chb-technologies.ch>
2019-10-03 14:41:12 +0400

    added docker file

--- src/app.js
+++ src/app.js
@@ -7,6 +7,8 @@ const app = express();
 app.use(logger('dev'));
 app.use(bodyparser.json());
 
+app.get('/greet/:name', (req, res) => res.send({ msg : `Hello ${req.params.name}`}));
+
 app.get('/', (req, res) => res.send('Welcome to Demo Node API'));
 
 app.listen(3000, () => console.log('Express is listening on port 3000'));
